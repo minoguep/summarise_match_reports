cbeeea23fed7363571c35d1dc090bfaef9361fb3
paul.minogue <paul.minogue@edgetier.com>
2020-03-28 23:14:15 +0000

    Update README and spelling in match_reports.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Using the Hugging Face BART model to summarise RTE's match reports from the 2019 All-Ireland Finals
 
-This repository contains the code which supports [my blog post](https://paulminogue.com) on text summarisation using.
+This repository contains the code which supports [my blog post](https://paulminogue.com/index.php/2020/03/28/using-the-hugging-face-bart-model-to-summarise-match-reports-from-the-2019-all-ireland-championship-finals/) on text summarisation using.
 If you want to read more about this model, you can do so [here](https://github.com/huggingface/transformers/releases)
--- match_reports.py
+++ match_reports.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 import requests
 from bs4 import BeautifulSoup
-summarizer = pipeline('summarization')
+summariser = pipeline('summarization')
 
 dublin_kerry_url = "https://www.rte.ie/sport/gaa/2019/0914/1075925-five-alive-o-dublin-dominate-kerry-to-make-gaa-history/"
 tipp_kilkenny_url = "https://www.rte.ie/sport/gaa/2019/0818/1069488-all-ireland-hurling-final-2019-kilkenny-0-20-tipperary-3-25/"
@@ -13,6 +13,6 @@ for url in [dublin_kerry_url, tipp_kilkenny_url]:
     match_report = soup.findAll('p')
     match_report_text = ' '.join([paragraph.text for paragraph in match_report])
 
-    match_summary = summarizer(match_report_text, min_length=100, max_length=500)
+    match_summary = summariser(match_report_text, min_length=100, max_length=500)
 
     print(match_summary[0]['summary_text'])
